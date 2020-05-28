0b07e60c236b76f771dcd4debf60101920a05a4a
Chinmay Maheshwari <chinmay12456789@gmail.com>
2020-05-28 16:45:43 +0530

    setup complete

--- online_exam/settings.py
+++ online_exam/settings.py
@@ -14,10 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR,'template')
-STATIC_DIR = os.path.join(BASE_DIR,'template')
-MEDIA_DIR = os.path.join(BASE_DIR,'template')
-
+TEMPLATE_DIR=os.path.join(BASE_DIR,'template')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+MEDIA_DIR = os.path.join(BASE_DIR,'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -58,7 +57,7 @@ ROOT_URLCONF = 'online_exam.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = MEDIA_DIR
+
+STATICFILES_DIRS = [STATIC_DIR,]
