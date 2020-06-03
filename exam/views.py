c318f2a41034ac9fb86945413670dbe7f434b28c
Chinmay Maheshwari <chinmay12456789@gmail.com>
2020-06-03 19:50:06 +0530

    SS upload success

--- exam/views.py
+++ exam/views.py
@@ -6,6 +6,16 @@ from .serializers import *
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+import base64
+from django.core.files.base import ContentFile
+
+
+def base64_file(data, name=None):
+    _format, _img_str = data.split(';base64,')
+    _name, ext = _format.split('/')
+    if not name:
+        name = _name.split(":")[-1]
+    return ContentFile(base64.b64decode(_img_str), name='{}.{}'.format(name, ext))
 
 def onlineexam(request,uid):
 	if request.method=='GET':
@@ -90,7 +100,8 @@ class StudentResponse(APIView):
 class PhotoUploadView(APIView):
 	def post(self, request,uid, *args, **kwargs):
 		ss = request.data['file']
+		print(request.data)
 		student_object = Student_Exam.objects.get(external_identifier = uid)
-		img_upload = ProcteredSS(student_exam=student_object,img=ss)
+		img_upload = ProcteredSS(student_exam=student_object,img=base64_file(ss))
 		img_upload.save()
 		return Response({'status':'ok'})
--- online_exam/settings.py
+++ online_exam/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = MEDIA_DIR
-STATIC_ROOT = STATIC_DIR
-#STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [STATIC_DIR,]
 
 #Email Setting
 EMAIL_HOST = 'smtp.gmail.com'
